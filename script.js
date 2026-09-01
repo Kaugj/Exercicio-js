71f383b7b8d1bd694cc2b142372b66c905462a26
Kauã Felipe Donizete de Oliveira <107350947+Kaugj@users.noreply.github.com>
2026-09-01 16:58:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,36 @@ function verificarPrioridade() {
           document.getElementById("Resultado").textContent = "Prioridade dseconhecida.";
 }
 }
+
+// atividade 4
+
+function verificarPrioridade() {
+     const entrada = document.getElementById("prioridade").value;
+     const prioridade = entrada.toLowerCase().trim();
+
+     let mensagem
+
+      switch (prioridade) {
+        case "urgente":
+          mensagem = "Atenção redobrada!";
+          break;
+      
+        case "alta":
+          mensagem = "Prioridade alta.";
+          break;
+      
+        case "media":
+          mensagem = "Prioridade média.";
+          break;
+      
+        case "baixa":
+          mensagem = "Prioridade baixa.";
+          break;
+      
+        default:
+          mensagem = "Prioridade dseconhecida.";
+}
+
+// Exibe a resposta na tela
+document.getElementById("Prioridade").innerText = mensagem;
+}
